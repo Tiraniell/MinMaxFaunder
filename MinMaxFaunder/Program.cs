85006a3930d7cde9cc727a0c214a3c6bf3fd281e
Tiraniell <93056140+Tiraniell@users.noreply.github.com>
2023-01-24 22:23:59 +0300

    Добавьте файлы проекта.

---
+++ MinMaxFaunder/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace MinMaxFaunder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Есть массив. Задача вывести исходны массив. Найти минимум и максимум. Найти среднее арифметическое
+
+            int maxValue = int.MinValue;
+            int minValue = int.MaxValue;
+            int arithmeticMean;
+
+            int[] array = new int[] { 13, 3, 435, 5, 63, -7, 8, 9, 3, 6, 12 };
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > maxValue)
+                {
+                    maxValue = array[i];
+                }
+
+                if (array[i] < minValue)
+                {
+                    minValue = array[i];
+                }
+            }
+
+            Console.WriteLine( "Min " + minValue );
+            Console.WriteLine( "Max " + maxValue );
+        }
+    }
+}
